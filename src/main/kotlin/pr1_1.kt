9b3c800ebfb33c69e18e2912e00a0f2314f7779d
meetp <meetmpatel20@gnu.ac.in>
2022-09-01 19:08:21 +0530

    Initial commit

---
+++ src/main/kotlin/pr1_1.kt
@@ -0,0 +1,20 @@
+
+fun main() {
+    val a: Int= 81
+    val b: Double =8.1
+    val c: Char='A'
+    val d: Boolean= true
+    val e: Long= 20012011081
+    val f: Short= -81
+    val g: Byte= 81
+
+    println("Int : $a")
+    println("Double : $b")
+    println("Char : $c")
+    println("Boolean : $d")
+    println("Long : $e")
+    println("Short : $f")
+    println("Byte : $g")
+
+
+}
